e3659fac4a44234a6b4433ac368e4e2d9aaec114
r.russo <rafael.porchera@sigtrans.com.br>
2024-05-15 11:15:41 -0300

    Add swagger and start to structure the application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle(`darwini-api`)
+    .setDescription(`Env: des - Serviço de administração medica`)
+    .setVersion(`1`)
+    .setContact(
+      'Rafael Russo',
+      'https://www.linkedin.com/in/rafael-russop/',
+      'rrussoporchera@gmail.com',
+    )
+    .setExternalDoc('Open Api: swagger-ui-json', '/swagger-ui-json')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    deepScanRoutes: true,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('swagger-ui', app, document);
+
+  app.enableCors();
   await app.listen(3000);
+  console.log(`🔥 listening on port 3000!`);
 }
 bootstrap();
